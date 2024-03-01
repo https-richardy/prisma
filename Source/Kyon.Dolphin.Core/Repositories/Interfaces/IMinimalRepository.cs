947f7f7f27b0cfe6a07c1f79ee2e72bc59da3080
https-richardy <=>
2024-03-01 20:23:03 -0300

    feat: add IMinimalRepository interface for basic CRUD operations

---
+++ Source/Kyon.Dolphin.Core/Repositories/Interfaces/IMinimalRepository.cs
@@ -0,0 +1,49 @@
+namespace Kyon.Dolphin.Core.Data.Repositories;
+
+/// <summary>
+/// Minimal repository interface for basic CRUD operations on entities.
+/// </summary>
+/// <typeparam name="TEntity">The type of entity managed by the repository.</typeparam>
+public interface IMinimalRepository<TEntity>
+{
+    /// <summary>
+    /// Asynchronously saves a new entity to the repository.
+    /// </summary>
+    /// <param name="entity">The entity to be saved.</param>
+    /// <returns>A task representing the asynchronous save operation.</returns>
+    Task SaveAsync(TEntity entity);
+
+    /// <summary>
+    /// Asynchronously deletes an existing entity from the repository.
+    /// </summary>
+    /// <param name="entity">The entity to be deleted.</param>
+    /// <returns>A task representing the asynchronous delete operation.</returns>
+    Task DeleteAsync(TEntity entity);
+
+    /// <summary>
+    /// Asynchronously updates an existing entity in the repository.
+    /// </summary>
+    /// <param name="entity">The entity to be updated.</param>
+    /// <returns>A task representing the asynchronous update operation.</returns>
+    Task UpdateAsync(TEntity entity);
+
+    /// <summary>
+    /// Asynchronously retrieves an entity from the repository by its integer identifier.
+    /// </summary>
+    /// <param name="id">The integer identifier of the entity.</param>
+    /// <returns>A task representing the asynchronous retrieval operation, returning the retrieved entity.</returns>
+    Task<TEntity> RetrieveByIdAsync(int id);
+
+    /// <summary>
+    /// Asynchronously retrieves an entity from the repository by its string identifier.
+    /// </summary>
+    /// <param name="id">The string identifier of the entity.</param>
+    /// <returns>A task representing the asynchronous retrieval operation, returning the retrieved entity.</returns>
+    Task<TEntity> RetrieveByIdAsync(string id);
+
+    /// <summary>
+    /// Asynchronously retrieves all entities from the repository.
+    /// </summary>
+    /// <returns>A task representing the asynchronous retrieval operation, returning a collection of entities.</returns>
+    Task<IEnumerable<TEntity>> RetrieveAllAsync();
+}
