7977a1270dbe83823f1234c94e55c277a9507c6f
https-richardy <=>
2024-03-01 21:24:27 -0300

    #feat: add OperationResult enum for indicating operation success or failure

---
+++ Source/Kyon.Dolphin.Core/Common/OperationResult.cs
@@ -0,0 +1,17 @@
+namespace Kyon.Dolphin.Core.Common;
+
+/// <summary>
+/// Represents the result of an operation, indicating whether the operation was successful or failed.
+/// </summary>
+public enum OperationResult
+{
+    /// <summary>
+    /// Indicates that the operation was successful.
+    /// </summary>
+    Success,
+
+    /// <summary>
+    /// Indicates that the operation failed.
+    /// </summary>
+    Failed
+}
