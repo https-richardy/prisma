c2c20174d03604af7846cb8ff0eee22a84825470
https-richardy <=>
2024-03-01 23:11:08 -0300

    chore: add foo class for testing purposes

---
+++ Tests/TestObjects/Foo.cs
@@ -0,0 +1,26 @@
+// Kyon - Open Source Initiative
+// Licensed under the MIT License
+
+namespace Kyon.Dolphin.Tests;
+
+/// <summary>
+/// Represents a sample entity for testing purposes.
+/// </summary>
+internal class Foo : Model<int>
+{
+    public string Name { get; set; } = string.Empty;
+    public int Age { get; set; }
+    public DateTime BirthDate { get; set; }
+
+    public Foo()
+    {
+        /* Default constructor */
+    }
+
+    public Foo(string name, int age, DateTime birthDate)
+    {
+        Name = name;
+        Age = age;
+        BirthDate = birthDate;
+    }
+}
