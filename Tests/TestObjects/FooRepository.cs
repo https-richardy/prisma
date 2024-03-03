51e9c1e4b57a36ed998adb8e9afd780e1072b1aa
https-richardy <=>
2024-03-03 19:41:27 -0300

    test: Added FooRepository class, implementing IRepository interfaces, for testing purposes specifically designed for the Foo entity.

---
+++ Tests/TestObjects/FooRepository.cs
@@ -0,0 +1,17 @@
+// Kyon - Open Source Initiative
+// Licensed under the MIT License
+
+namespace Kyon.Dolphin.Tests;
+
+/// <summary>
+/// Represents a repository for testing purposes specifically design for the <see cref="Foo"/> entity.
+/// </summary>
+/// <remarks>
+/// This repository is implemented based on the <see cref="Repository{TEntity, Tkey, TDbContext}"/>
+/// and <see cref="IRepository{TEntity}"/> interfaces.
+/// </remarks>
+public class FooRepository : Repository<Foo, int, TestDbContext>, IRepository<Foo>
+{
+    public FooRepository(TestDbContext dbContext)
+        : base(dbContext) {  }
+}
