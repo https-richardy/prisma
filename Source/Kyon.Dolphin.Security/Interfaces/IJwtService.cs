ab086533bb08e51ab0d44904cd2c4dc3f7a9fb8c
https-richardy <=>
2024-03-08 13:48:10 -0300

    docs: Add documentation to IJwtService interface.

--- Source/Kyon.Dolphin.Security/Interfaces/IJwtService.cs
+++ Source/Kyon.Dolphin.Security/Interfaces/IJwtService.cs
@@ -5,7 +5,15 @@ using System.Security.Claims;
 
 namespace Kyon.Dolphin.Security;
 
+/// <summary>
+/// Interface for a service responsible for creating JSON Web Token (JWT) based on provided user information (claims identity).
+/// </summary>
 public interface IJwtService
 {
+    /// <summary>
+    /// Generates a JWT based on the provided claims identity.
+    /// </summary>
+    /// <param name="claimsIdentity">User information and claims to be included in the JWT.</param>
+    /// <returns>Generated JWT as a string.</returns>
     string GenerateToken(ClaimsIdentity claimsIdentity);
 }
