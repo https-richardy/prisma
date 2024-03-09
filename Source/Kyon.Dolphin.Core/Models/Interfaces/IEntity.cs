da400834e0b90e8ae1d1a64dc0d2422dc575ca53
https-richardy <=>
2024-03-08 23:01:36 -0300

    docs: added XML documentation for IEntity<TKey> interface. Added XML documentation for the Id property in the IEntity<TKey> interface.

--- Source/Kyon.Dolphin.Core/Models/Interfaces/IEntity.cs
+++ Source/Kyon.Dolphin.Core/Models/Interfaces/IEntity.cs
@@ -9,5 +9,8 @@ namespace Kyon.Dolphin.Core.Models;
 /// <typeparam name="TKey">The type of the primary key</typeparam>
 public interface IEntity<TKey>
 {
+    /// <summary>
+    /// Gets the unique identifier for the entity.
+    /// </summary>
     TKey Id { get; }
 }
