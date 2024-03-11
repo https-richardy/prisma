6bfc0c5143c05997ca3f567127f1be522fa540b4
https-richardy <=>
2024-03-11 11:06:55 -0300

    docs: enhance documentation for the base Model class in the Prisma.Core.Models namespace. The revised documentation provides additional context and explanations.

--- Source/Prisma.Core/Models/Model.cs
+++ Source/Prisma.Core/Models/Model.cs
@@ -8,9 +8,15 @@ namespace Prisma.Core.Models;
 # pragma warning disable CS8618
 
 /// <summary>
-/// Represents a base model for entities 
+/// Represents a base model for entities with a primary key of type <typeparamref name="TKey"/>.
 /// </summary>
-/// <typeparam name="TKey">The type of the entity's indentifier.</typeparam>
+/// <typeparam name="TKey">The type of the entity's identifier.</typeparam>
+/// <remarks>
+/// This base model provides a common structure for entities by introducing a unique identifier property.
+/// The identifier, represented by the property <see cref="Id"/>, is of type <typeparamref name="TKey"/>.
+/// Entities deriving from this base model can leverage the standardized identifier property for
+/// consistent handling of primary keys across the application.
+/// </remarks>
 public abstract class Model<TKey> : IEntity<TKey>
 {
     /// <summary>
