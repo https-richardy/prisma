d88a0fd76e8ea0e1898abcf64e8a70fd7cf76094
https-richardy <=>
2024-03-11 12:30:27 -0300

    docs: enhance documentation for JwtServiceExtensions. Improved the documentation for the JwtServiceExtensions class, providing comprehensive details about the functionality and purpose of each method. The documentation now includes a clearer explanation of the AddJwtBearer extension methods, their tasks, and the flexibility they offer for integrating JWT services into ASP.NET Core applications. Additionally, it describes the customization options available through the AddJwtBearer method with custom options, facilitating better understanding and usage.

--- Source/Prisma.Security/JwtServiceExtensions.cs
+++ Source/Prisma.Security/JwtServiceExtensions.cs
@@ -21,11 +21,46 @@ namespace Prisma.Security.Extensions;
 /// </remarks>
 public static class JwtServiceExtensions
 {
+
     /// <summary>
-    /// Adds the JWT service to the service collection.
+    /// Adds the JWT service to the service collection, configures authentication, and registers authorization.
     /// </summary>
     /// <param name="services">The service collection to add the JWT service to.</param>
     /// <param name="configuration">The configuration providing access to application settings.</param>
+    /// <remarks>
+    /// <para>
+    /// The <c>AddJwtBearer</c> method is an extension method for <see cref="IServiceCollection"/>,
+    /// designed to simplify the integration of the JWT service into the application's dependency injection container.
+    /// </para>
+    /// <para>
+    /// It performs the following tasks:
+    /// <list type="bullet">
+    /// <item>
+    /// <description>Registers the <see cref="IJwtService"/> interface with the implementation of <see cref="JwtService"/> in the dependency injection container.</description>
+    /// </item>
+    /// <item>
+    /// <description>
+    /// Retrieves the JWT secret key from the application settings using the provided <paramref name="configuration"/> and converts it to a byte array.
+    /// </description>
+    /// </item>
+    /// <item>
+    /// <description>Configures the authentication middleware with JWT Bearer authentication as the default scheme for both authentication and challenge.</description>
+    /// </item>
+    /// <item>
+    /// <description>
+    /// Configures the <see cref="TokenValidationParameters"/> for the JWT Bearer authentication, including options such as validating the issuer signing key,
+    /// disabling issuer and audience validation, and setting the clock skew to allow for a time difference between the server and the client.
+    /// </description>
+    /// </item>
+    /// <item>
+    /// <description>Registers the authentication middleware with the service collection.</description>
+    /// </item>
+    /// <item>
+    /// <description>Registers the authorization services with the service collection.</description>
+    /// </item>
+    /// </list>
+    /// </para>
+    /// </remarks>
 
     #pragma warning disable CS8604
     public static void AddJwtBearer(this IServiceCollection services, IConfiguration configuration)
@@ -57,6 +92,49 @@ public static class JwtServiceExtensions
     /// <param name="services">The service collection to add the JWT service to.</param>
     /// <param name="configuration">The configuration providing access to application settings.</param>
     /// <param name="configureOptions">An action to configure the JWT options.</param>
+    /// <remarks>
+    /// <para>
+    /// The <c>AddJwtBearer</c> method is an extension method for <see cref="IServiceCollection"/>,
+    /// designed to simplify the integration of the JWT service into the application's dependency injection container with custom options.
+    /// </para>
+    /// <para>
+    /// It allows customization of the JWT service through the <paramref name="configureOptions"/> action, providing a flexible way
+    /// to set properties such as the cryptographic key, security algorithm, and expiration time for the JWT.
+    /// </para>
+    /// <para>
+    /// This method performs the following tasks:
+    /// <list type="bullet">
+    /// <item>
+    /// <description>Creates an instance of <see cref="JwtOptions"/> to store and configure the options for the JWT service.</description>
+    /// </item>
+    /// <item>
+    /// <description>Retrieves the JWT secret key from the application settings using the provided <paramref name="configuration"/> and converts it to a byte array.</description>
+    /// </item>
+    /// <item>
+    /// <description>Invokes the <paramref name="configureOptions"/> action to allow customization of the JWT options.</description>
+    /// </item>
+    /// <item>
+    /// <description>Registers the <see cref="IJwtService"/> interface with the implementation of <see cref="JwtService"/> in the dependency injection container,
+    /// providing the configuration and options.</description>
+    /// </item>
+    /// <item>
+    /// <description>Configures the authentication middleware with JWT Bearer authentication as the default scheme for both authentication and challenge.</description>
+    /// </item>
+    /// <item>
+    /// <description>
+    /// Configures the <see cref="TokenValidationParameters"/> for the JWT Bearer authentication, including options such as validating the issuer signing key,
+    /// disabling issuer and audience validation, and setting the clock skew to allow for a time difference between the server and the client.
+    /// </description>
+    /// </item>
+    /// <item>
+    /// <description>Registers the authentication middleware with the service collection.</description>
+    /// </item>
+    /// <item>
+    /// <description>Registers the authorization services with the service collection.</description>
+    /// </item>
+    /// </list>
+    /// </para>
+    /// </remarks>
     public static void AddJwtBearer(this IServiceCollection services, IConfiguration configuration, Action<JwtOptions> configureOptions)
     {
         var options = new JwtOptions();
